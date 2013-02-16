1f761abeb4c279cd015b69ae6462bb56f3f1b01e
Sávio S. Dias <wolfdragon2@gmail.com>
2013-02-16 13:01:27 -0200

    Criado projeto com Windows Forms

---
+++ OCR/OCR/MultiLayerPerceptronNetwork.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neural_Network
+{
+    class MultiLayerPerceptronNetwork
+    {
+        // ------------------- 1.VARIABLES AND PROPERTIES -------------------
+        public double LearningRate { get; set; }
+
+        public double[] Input { get; set; }
+        public Neuron[] HiddenLayer { get; set; }
+        public Neuron[] OutLayer { get; set; }
+
+        public double[] ExpectedOutput { get; set; }
+
+        //  ------------------- 2.CONSTRUCTORS -------------------
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hiddenLayerAmount"></param>
+        /// <param name="outLayerAmount"></param>
+        /// <param name="learningRate"></param>
+        public MultiLayerPerceptronNetwork(int hiddenLayerAmount, int outLayerAmount, double learningRate)
+        {
+            LearningRate = learningRate;
+
+            // Allocating Hidden Layer
+                HiddenLayer = new Neuron[hiddenLayerAmount];
+                // Instantiating Neurons from Hidden Layer
+                for (int i = 0; i < HiddenLayer.Length; i++)
+                    HiddenLayer[i] = new Neuron(LearningRate);
+            //
+
+            // Allocating Output Layer
+                OutLayer = new Neuron[outLayerAmount];
+                // Instantiating Neurons from Output Layer
+                for (int i = 0; i < OutLayer.Length; i++)
+                    OutLayer[i] = new Neuron(LearningRate);
+            //
+            
+            // Setting Weights manually
+                SetManuallyWeights();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="hiddenLayerAmount"></param>
+        /// <param name="outLayerAmount"></param>
+        /// <param name="learningRate"></param>
+        public MultiLayerPerceptronNetwork(double[] input, int hiddenLayerAmount, int outLayerAmount, double learningRate)
+        {
+            Input = input;
+            LearningRate = learningRate;
+
+            // Allocating Hidden Layer
+                HiddenLayer = new Neuron[ hiddenLayerAmount ];
+                // Instantiating Neurons from Hidden Layer
+                for (int i = 0; i < HiddenLayer.Length; i++)
+                    HiddenLayer[i] = new Neuron(LearningRate);
+            //
+
+            // Allocating Output Layer
+                OutLayer = new Neuron[ outLayerAmount ];
+                // Instantiating Neurons from Output Layer
+                for (int i = 0; i < OutLayer.Length; i++)
+                    OutLayer[i] = new Neuron(LearningRate);
+            //
+
+            // Setting Weights manually
+            SetManuallyWeights();
+
+            // Set input on the hidden layer
+            SetInputOnHiddenLayer();
+        }
+
+        //  ------------------- 3.GETTERS & SETTERS -------------------
+        /// <summary>
+        /// Retorna o vetor com as saídas de cada neurônio
+        /// </summary>
+        /// <returns></returns>
+        public double[] GetOutputs()
+        {
+            double[] output = new double[ OutLayer.Length ];
+            for (int i = 0; i < OutLayer.Length; i++)
+                output[i] = OutLayer[i].Output;
+
+            return output;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public double[] GetErrors()
+        {
+            double[] errors = new double[ HiddenLayer.Length + OutLayer.Length ];
+            for (int i = 0; i < OutLayer.Length; i++)
+                errors[i] = OutLayer[i].Error;
+            for (int i = 0; i < HiddenLayer.Length; i++)
+                errors[ i + OutLayer.Length ] = HiddenLayer[i].Error;
+
+            return errors;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public double[] GetBackPropagatedErrors()
+        { 
+            double[] errors = new double[ HiddenLayer.Length + OutLayer.Length ];
+            for (int i = 0; i < OutLayer.Length; i++)
+                errors[i] = OutLayer[i].BackPropagatedError;
+            for (int i = 0; i < HiddenLayer.Length; i++)
+                errors[i + OutLayer.Length] = HiddenLayer[i].BackPropagatedError;
+
+            return errors;
+        }
+
+        /// <summary>
+        /// Função para testes da rede que define os pesos manualmente
+        /// </summary>
+        private void SetManuallyWeights()
+        {
+            HiddenLayer[0].Weights = new double[] { -0.2, 0.5, -0.65 };
+            HiddenLayer[1].Weights = new double[] { 0.3, 0.72, 0.8 };
+            OutLayer[0].Weights = new double[] { -0.5, 1.0, -1.0 };
+        }
+
+        /// <summary>
+        /// Define a entrada nos neurônios da camada oculta
+        /// </summary>
+        private void SetInputOnHiddenLayer()
+        {
+            // Put values "input" in the "position" layer
+            if (Input != null)
+                for (int i = 0; i < HiddenLayer.Length; i++)
+                    HiddenLayer[i].Input = Input;
+            else throw new Exception("input parameter invalid for function task!");
+        }
+
+        /// <summary>
+        /// Define a entrada, input, nos neurônios da camada de saída
+        /// </summary>
+        /// <param name="input"></param>
+        private void SetInputOnOutLayer(double[] input)
+        {
+            if (input != null)
+                for (int i = 0; i < OutLayer.Length; i++)
+                    OutLayer[i].Input = input;
+            else throw new Exception("input parameter invalid for function task!");
+        }
+
+        //  ------------------- 4.FUNCTIONAL METHODS -------------------
+        /// <summary>
+        /// Calcula o Erro para cada neurônio na camada oculta
+        /// </summary>
+        private void CalculateHiddenLayerErrors()
+        {
+            for (int i = 0; i < HiddenLayer.Length; i++)
+            { 
+                // Calculating Error
+                double sum = 0.0;
+                for(int j = 0; j < OutLayer.Length; j++)
+                    sum += OutLayer[j].BackPropagatedError * OutLayer[j].Weights[i + 1];
+                HiddenLayer[i].Error = sum;
+
+                // Calculating Back Propagated Error
+                HiddenLayer[i].CalculateBackPropagatedError();
+            }
+        }
+
+        /// <summary>
+        /// Realiza o processo de propagação (forward) na rede
+        /// </summary>
+        public void Forward()
+        {
+            // Do forward process in hidden layer
+                // Vector to store the neuron layer outs
+                double[] tempOut = new double[ HiddenLayer.Length ];
+
+                // Forward process in current hidden layer and store the outputs
+                for (int i = 0; i < HiddenLayer.Length; i++)
+                {
+                    HiddenLayer[i].Forward();
+                    tempOut[i] = HiddenLayer[i].Output;
+                }
+
+                // Set inputs from Hidden Layer on Output Layer
+                SetInputOnOutLayer(tempOut);
+            //
+
+            // Do forward process in the output layer
+            for (int i = 0; i < OutLayer.Length; i++)
+                OutLayer[i].Forward();
+        }
+
+        /// <summary>
+        /// Realiza o processo de retro-propagação (backward) na rede
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <param name="expectedOutput"></param>
+        public void Backward(double[] inputs, double[] expectedOutput)
+        {
+            // If expected output vector size equals to output layer size
+            if (expectedOutput.Length == OutLayer.Length)
+            {
+                Input = inputs;
+                SetInputOnHiddenLayer();
+                ExpectedOutput = expectedOutput;
+
+                // Call to Forward method to realize the respective calculus
+                Forward();
+
+                // Calculating Error and Back Propagated Error
+                    // Output Layer
+                    for (int i = 0; i < OutLayer.Length; i++)
+                    {
+                        OutLayer[i].ExpectedOutput = expectedOutput[i];
+                        OutLayer[i].CalculateError();
+                        OutLayer[i].CalculateBackPropagatedError();
+                    }
+
+                    // Hidden Layer
+                    CalculateHiddenLayerErrors();
+                //
+
+                // Weights adjustment
+                    // Hidden Layer
+                    for (int i = 0; i < HiddenLayer.Length; i++)
+                        HiddenLayer[i].WeightsAdjustment();
+
+                    // Output Layer
+                    for (int i = 0; i < OutLayer.Length; i++)
+                        OutLayer[i].WeightsAdjustment();
+                //
+            }
+            else throw new Exception("Expected Output is in a different size of Output Layer´s Network");
+        }
+    }
+}
---
+++ OCR/OCR/Neuron.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neural_Network
+{
+    class Neuron
+    {
+        #region 1.VARIABLES AND PROPERTIES
+        // ------------------- 1.VARIABLES AND PROPERTIES ------------------- 
+        // Constants
+        private static double bias = 1.0;
+
+        // Forward instances with its properties
+        public double LearningRate { get; set; }
+        public double[] Input { get; set; }  //private double[] inputs;
+
+        // The bias weight is found in the first position (Weights[0])
+        public double[] Weights { get; set; }   //private double[] weights;
+
+        // Output from the Neuron
+        public double Output { get; set; }  //private double output;
+
+        // Backward instances with its properties
+        public double ExpectedOutput { get; set; }  //private double expectedOutput;
+        public double Error { get; set; }   //private double error;
+        public double BackPropagatedError { get; set; } //private double backPropagatedError;
+        #endregion
+
+        #region 2.CONSTRUCTORS
+        //  ------------------- 2.CONSTRUCTORS -------------------
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="learningRate"></param>
+        public Neuron(double learningRate){
+            LearningRate = learningRate;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="learningRate"></param>
+        public Neuron(double[] input, double learningRate)
+        {
+            Input = input;
+            //SortWeights();
+            LearningRate = learningRate;
+        }
+        #endregion
+
+        //  ------------------- 3.PROPERTIES ------------------- 
+        public static double Bias
+        {
+            get { return bias; }
+            set { bias = value; }
+        }
+
+        //  ------------------- 4.FUNCTIONAL METHODS ------------------- 
+        /// <summary>
+        /// Sorteia o peso para cada entrada incluindo o bias
+        /// </summary>
+        private void SortWeights()
+        {
+            // Instantiating weights vector
+            // First position to store bias weight
+            Weights = new double[Input.Length + 1];
+
+            // Instantiating the class to random numbers generation
+            Random random = new Random( DateTime.Now.Millisecond );
+
+            // Filling the weights vector
+            for (int i = 0; i < Weights.Length; i++)
+                Weights[i] = random.Next(-1000, 1000) / 1000.0;
+        }
+
+        #region Error's calculus
+        /// <summary>
+        /// 
+        /// </summary>
+        public void CalculateError()
+        {
+            // Error calculation
+            Error = ExpectedOutput - Output;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void CalculateBackPropagatedError()
+        {
+            // Back propagated error calculation
+            BackPropagatedError = (1.0 - Output * Output) * Error;
+        }
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void WeightsAdjustment()
+        {
+            Weights[0] += LearningRate * Bias * BackPropagatedError;
+            for (int i = 1; i < Weights.Length; i++)
+                Weights[i] += LearningRate * Input[i - 1] * BackPropagatedError;
+        }
+
+        /// <summary>
+        /// Realiza o processo de propagação (forward) no neurônio
+        /// </summary>
+        public void Forward()
+        {
+            // Performing the sum of the inputs with the weights
+            double sum = Bias * Weights[0];
+            for ( int i = 0; i < Input.Length; i++ )
+                sum += Input[ i ] * Weights[ i + 1 ];
+
+            // Validation function
+            Output = Math.Tanh( sum );
+        }
+
+        /// <summary>
+        /// Realiza o processo de retro-propagação (backward) no neurônio
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <param name="expectedOutput"></param>
+        public void Backward( double[] inputs, double expectedOutput )
+        {
+            Input = inputs;
+            ExpectedOutput = expectedOutput;
+
+            // Call to Forward method to realize the respective calculus
+            Forward();
+
+            // Calculate Error and Back Propagated Error
+            CalculateError();
+            CalculateBackPropagatedError();
+
+            // Weights adjustment
+            WeightsAdjustment();
+        }
+    }
+}
